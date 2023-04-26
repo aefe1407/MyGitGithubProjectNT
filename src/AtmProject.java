ee25b9ec0e7b6071ee171391d1f9530097ce14e7
Ahmet EFE <aefe1407@gmail.com>
2023-04-26 22:04:37 +0300

    version4

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -7,5 +7,7 @@ public class AtmProject {
         //version2
         System.out.println("Learn Java earn money");
         //version3
+        System.out.println("Git öğrenmek çok kolay:)");
+        //version4
     }
 }
